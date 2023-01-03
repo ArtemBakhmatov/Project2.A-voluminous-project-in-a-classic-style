61b9f06300b6dd8d8bc9c22922a4d24c3f102a5e
Artem <ip.bakhmatov@mail.ru>
2023-01-03 20:37:54 +0300

    Подгружаем элементы с сервера

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,10 +1,8 @@
-//import { checkNumInputs } from "./checkNumInputs";
+import { postData } from "../services/requests";
 const forms = () => {
     const form = document.querySelectorAll('form'),
         inputs = document.querySelectorAll('input'),
         upload = document.querySelectorAll('[name="upload"]');
-
-    //checkNumInputs('input[name="user_phone"]');
 
     const message = {
         loading: 'Загрузка...',
@@ -19,15 +17,6 @@ const forms = () => {
         designer: 'assets/server.php',
         question: 'assets/question.php'
     }
-
-    const postData = async (url, data) => {
-        let res = await fetch(url, {
-            method: "POST",
-            body: data
-        });
-
-        return await res.text();
-    };
 
     const clearInputs = () => {
         inputs.forEach(item => {
